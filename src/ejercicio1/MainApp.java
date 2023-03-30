11feb0703cefea9f9b71dd54be1458ba5d6ea8d5
burnout131 <marcrg07@gmail.com>
2023-03-30 12:18:46 +0200

    Commit inicial

---
+++ src/ejercicio1/MainApp.java
@@ -0,0 +1,20 @@
+package ejercicio1;
+
+import java.awt.EventQueue;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Saludador frame = new Saludador();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
---
+++ src/ejercicio1/Saludador.java
@@ -0,0 +1,52 @@
+package ejercicio1;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Saludador extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Create the frame.
+	 */
+	public Saludador() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setLayout(null);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+
+		JLabel lblNewLabel = new JLabel("Escribe un nombre para saludar");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(123, 32, 189, 26);
+		contentPane.add(lblNewLabel);
+
+		textField = new JTextField();
+		textField.setBounds(91, 94, 236, 33);
+		contentPane.add(textField);
+		textField.setColumns(10);
+
+		JButton btnNewButton = new JButton("¡Saludar!");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "¡Hola " + textField.getText() + "!");
+			}
+		});
+		btnNewButton.setBounds(178, 174, 89, 23);
+		contentPane.add(btnNewButton);
+	}
+}
